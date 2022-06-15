8be43653cc4c0dcadbe3d9c19dc824eb662016bb
Иван <lostnemo@rambler.ru>
2022-06-15 17:08:56 +0300

    Добавляет функцию показа предупреждения в модуль утилитарных функций

--- js/util.js
+++ js/util.js
@@ -1,7 +1,9 @@
 const Keys = {
   ESC: 'Esc',
   ESCAPE: 'Escape',
-}
+};
+
+const ALERT_SHOW_TIME = 5000;
 
 const getRandomInt = (min, max) => {
   if (min < 0 || max < 0) {
@@ -63,4 +65,26 @@ const findDuplicate = (array, element) => {
 };
 
 
-export { getRandomInt, isLengthValid, getRandomArrayElement, getUniqueRandomInt, isEscEvent, isConsistLettersNumbers, findDuplicate, isFocused };
+const showAlert = (error) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.lineHeight = '40px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+  alertContainer.textContent = error;
+  
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+}
+
+
+export { getRandomInt, isLengthValid, getRandomArrayElement, getUniqueRandomInt, isEscEvent, isConsistLettersNumbers, findDuplicate, isFocused, showAlert };
