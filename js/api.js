15bfba6b4b6983b33e1bfe1935bddacd5e49b9a9
Иван <lostnemo@rambler.ru>
2022-06-15 17:12:38 +0300

    Добавляет функцию для отправки данных в модуль взаимодействия с сервером

--- js/api.js
+++ js/api.js
@@ -16,3 +16,28 @@ const getData = (onSuccess, onError) => {
       onError(`Ошибка запроса к серверу "${err.message}". Показаны данные для тестирования`);      
     });
 };
+
+
+const sendData = (onSuccess, onFail, body) => {
+  fetch(
+    'https://23.javascript.pages.academy/kekstagram',
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+    })
+    .catch((err) => {
+      onFail(`Не удалось отправить форму. Возникла ошибка - "${err.message}". Попробуйте ещё раз`);
+    });
+};
+
+
+export { getData , sendData }
+
