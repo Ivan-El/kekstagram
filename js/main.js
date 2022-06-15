5b2323c027a3d1d31525f58b85c2443473e8c1cd
Иван <lostnemo@rambler.ru>
2022-06-15 17:02:55 +0300

    Настраивает в точке входа получение и отрисовку данных с сервера, а вслучае ошибки отрисовки шаблонных данных

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+import { randomPosts } from './random-data.js';
 import { renderGallery } from './gallery.js';
 import { initUpload } from './upload.js';
+import { getData } from './api.js';
 
-renderGallery();
+
+getData((previews) => {renderGallery(previews);
+},(err)=> renderGallery(randomPosts, err));
 initUpload();
