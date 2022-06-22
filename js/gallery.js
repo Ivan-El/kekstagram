499cd6a0399f67aa3be055226e02dc6821201675
Иван <lostnemo@rambler.ru>
2022-06-22 17:03:39 +0300

    Меняет структуру модуля галлерии, выносит функции в соответствующие модули затем собирает их в промис

--- js/gallery.js
+++ js/gallery.js
@@ -1,36 +1,20 @@
 import { showAlert } from './util.js'
 import { renderPreview } from './preview.js';
-import { openUserModal, closeUserModal } from './modal.js';
-import { renderComments, renderFullscreenPictures } from './fullscreen.js';
+import { renderFullscreen } from './fullscreen.js';
+import { applyFilter } from './filter.js';
 
 
-const renderGallery = (posts, err) => { 
-  renderPreview(posts);
-
+const renderGallery = (posts, filter, err) => {
   if (err) {
     showAlert(err);
-  } 
+  }
 
-  // На время убираем счетчики 
-  const fotoCommentCount = document.querySelector('.social__comment-count');
-  const fotoCommentLoader = document.querySelector('.comments-loader');
-  fotoCommentCount.classList.add('hidden');
-  fotoCommentLoader.classList.add('hidden');
-  //   
-  const previews = document.querySelectorAll('.picture');
-  const fullScreenContainer = document.querySelector('.big-picture');
-  const fullScreenContainerClose = fullScreenContainer.querySelector('.big-picture__cancel');
-
-  const onPreviewClick = (previewElement) => {
-    return () => {
-      openUserModal(fullScreenContainer);            
-      renderFullscreenPictures(previewElement, fullScreenContainer); 
-      renderComments(previewElement);    
-    } 
-  };
-
-  previews.forEach((preview, previewIndex) => preview.addEventListener('click', onPreviewClick(posts[previewIndex])));
-  fullScreenContainerClose.addEventListener('click', () => closeUserModal(fullScreenContainer));
+  new Promise(resolve => {
+    resolve(applyFilter(posts, filter))
+  }).then((filtered) => {
+    renderPreview(filtered);
+    renderFullscreen(filtered);
+  });
 }
 
 export { renderGallery }
