52b9f3ca28e848bd975aa720f5b3418e52c10632
Иван <lostnemo@rambler.ru>
2022-06-22 17:12:31 +0300

    Добавляет возможность просмостр загружаемого изображения и его настройку

--- js/upload.js
+++ js/upload.js
@@ -7,13 +7,37 @@ import { openUserModal, closeUserModal } from './modal.js';
 const uploadInput= document.querySelector('#upload-file');
 const uploadInputClose = document.querySelector('#upload-cancel');
 const imgEditForm = document.querySelector('.img-upload__overlay');
+const preview = document.querySelector('.img-upload__preview img');
+const effectPreviews = document.querySelectorAll('.effects__preview');
+
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 
 const startUpload = () => {
-  openUserModal(imgEditForm);
-  initScale();
-  initEffect();
-  initForm();
+  const file = uploadInput.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => {
+    return fileName.endsWith(it);
+  });
+
+
+  new Promise((resolve) => {
+    if (matches) {
+      const reader = new FileReader();
+
+      reader.addEventListener('load', () => {
+        preview.src = reader.result;
+        effectPreviews.forEach((element) => element.style.backgroundImage = `url(${reader.result})`);
+        resolve();
+      });
+
+      reader.readAsDataURL(file);
+    }
+  }).then(() => openUserModal(imgEditForm))
+    .then(() => initScale())
+    .then(() => initEffect())
+    .then(() => initForm())
 };
 
 
@@ -22,10 +46,11 @@ const cancelUpload = () => {
   removeScale();
   removeEffect();
   removeForm();
-}
+};
+
 
 const initUpload = () => {
-  uploadInput.addEventListener('click', startUpload);
+  uploadInput.addEventListener('change', startUpload);
   uploadInputClose.addEventListener('click', cancelUpload);
 }
 
